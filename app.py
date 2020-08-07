e4b3dbab688df22020895decdd544ef29fd50b38
Esteban Solorzano <estebansolorzano27@gmail.com>
2020-08-07 17:59:55 -0500

    Click validations for every option

--- app.py
+++ app.py
@@ -2,24 +2,48 @@ import click
 from text_detection import TextDetection
 
 @click.command()
-@click.option('-i', '--image', required=True, help='path to input image to be OCR\'d')
-@click.option('-o', '--output', help='')
-@click.option('-f', '--file_format', help='')
-@click.option('-m', '--min-conf', default=0, help='mininum confidence value to filter weak text detection')
-@click.option('-c', '--complete-row', default=False, help='')
-@click.option('-d', '--distance', default=10, help='')
-def ocr(image, output, file_format, min_conf, complete_row, distance):
-
-    detection = TextDetection(
+@click.option(
+    '-i', '--image',
+    prompt=True, required=True,
+    type=click.Path(dir_okay=False, exists=True),
+    help='Input image to be OCR\'d.')
+@click.option(
+    '-o', '--output',
+    type=click.Path(file_okay=False, exists=True),
+    help='Path to the output image and its content.')
+@click.option(
+    '-f', '--file_format',
+    default="csv",
+    show_default=True,
+    type=click.Choice(["json", "csv"]),
+    help='Format type to the result content.')
+@click.option(
+    '-m', '--min-conf',
+    default=0,
+    show_default=True,
+    type=click.IntRange(min=0, max=100),
+    help='Mininum confidence value to filter weak text detection.')
+@click.option(
+    '--line/--word',
+    default=False,
+    show_default=True,
+    help='Determines whether to extract words or rows of text.')
+@click.option(
+    '-d', '--distance',
+    default=10,
+    show_default=True,
+    type=click.IntRange(min=0, max=20),
+    help='Max distance between words in the same row, it\'s used if --line is used.')
+def ocr(image, output, file_format, min_conf, line, distance):
+    word_detection = TextDetection(
         image,
         output,
         file_format=file_format,
         min_confidence=min_conf,
-        complete_row=complete_row,
-        distance=distance
+        line=line, distance=distance
     )
     
-    detection.detect()
+    word_detection.detect()
 
 if __name__ == '__main__':
     ocr()
